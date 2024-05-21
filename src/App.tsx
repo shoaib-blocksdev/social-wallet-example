b4ca73c804d78a6cd0aec629670afded9b2f1dcd
Shahzaib Anwar <89007917+shoaib-blocksdev@users.noreply.github.com>
2024-05-21 11:46:16 +0500

    add tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+import {useState} from 'react';
 import './App.css'
 import {useWallet} from "cloud-social-wallet";
+import { Tooltip } from 'react-tooltip'
 
 function App() {
     return <WalletApp/>
 }
 
 function WalletApp() {
+    const [copied, setCopied] = useState(false)
     const {address, logout, login} = useWallet()
+    const copyToClipboad = () =>{
+        navigator.clipboard.writeText(address)
+        setCopied(true);
+    }
+
     return (
         <>
             <h1>Cloud wallet example</h1>
             {
                 address ? <>
-                    <p>{address}</p>
+                    <Tooltip id="tooltip-anchor-hide" />
+                    <button
+                        data-tooltip-id="tooltip-anchor-hide"
+                        data-tooltip-content={copied ? "Copied" : 'Click to copy'}
+                        data-tooltip-delay-hide={500}
+                        onBlur={()=> setCopied(false)}
+                        onClick={copyToClipboad}>{address}</button>
                     <button onClick={logout}>Disconnect</button>
                 </> : <>
                     <button onClick={() => login('google')}>Google Login</button>
